2e0a2263e585de675d1469ed0d93df108f13190b
Jocelyn Velarde <a01285780@itesm.mx>
2022-06-24 17:44:19 -0500

    background image errores solved with grid too

--- lib/MyGridScreen.dart
+++ lib/MyGridScreen.dart
@@ -11,142 +11,158 @@ class _MyGridScreenState extends State<MyGridScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("VOLTEC App Inventario"),
-        backgroundColor: Colors.blue,
-      ),
-      body: Center(
-          child: GridView.extent(
-        primary: false,
-        padding: const EdgeInsets.all(16),
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-        maxCrossAxisExtent: 200.0,
-        children: <Widget>[
-          Container(
-            padding:
-                const EdgeInsets.only(top: 140, bottom: 0, left: 0, right: 0),
-            child: Stack(children: [
-              Container(
-                width: 190,
-                height: 20,
-                color: Colors.white.withOpacity(0.6),
-                child: Text(
-                  "Herramientas",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 18),
+        appBar: AppBar(
+          title: Text("VOLTEC App Inventario"),
+          backgroundColor: Colors.blue,
+        ),
+        body: Stack(
+          children: [
+            Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+              image: AssetImage("assets/Stitch.png"),
+              fit: BoxFit.cover,
+            ))),
+            Center(
+                //Column(
+                //children[
+                //Center(todo lo de gridview), Container(lo de la imagen)])
+                child: GridView.extent(
+              primary: false,
+              padding: const EdgeInsets.all(16),
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              maxCrossAxisExtent: 200.0,
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.only(
+                      top: 140, bottom: 0, left: 0, right: 0),
+                  child: Stack(children: [
+                    Container(
+                      width: 190,
+                      height: 20,
+                      color: Colors.white.withOpacity(0.6),
+                      child: Text(
+                        "Herramientas",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    )
+                  ]),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/Herramientas.jpeg"),
+                        fit: BoxFit.cover),
+                  ),
                 ),
-              )
-            ]),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage("assets/Herramientas.jpeg"),
-                  fit: BoxFit.cover),
-            ),
-          ),
-          Container(
-            padding:
-                const EdgeInsets.only(top: 140, bottom: 0, left: 0, right: 0),
-            child: Stack(children: [
-              Container(
-                width: 190,
-                height: 20,
-                color: Colors.white.withOpacity(0.6),
-                child: Text(
-                  "Consumibles",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 18),
+                Container(
+                  padding: const EdgeInsets.only(
+                      top: 140, bottom: 0, left: 0, right: 0),
+                  child: Stack(children: [
+                    Container(
+                      width: 190,
+                      height: 20,
+                      color: Colors.white.withOpacity(0.6),
+                      child: Text(
+                        "Consumibles",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    )
+                  ]),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/Refacciones.jpeg"),
+                        fit: BoxFit.cover),
+                  ),
                 ),
-              )
-            ]),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage("assets/Refacciones.jpeg"),
-                  fit: BoxFit.cover),
-            ),
-          ),
-          Container(
-            padding:
-                const EdgeInsets.only(top: 140, bottom: 0, left: 0, right: 0),
-            child: Stack(children: [
-              Container(
-                width: 190,
-                height: 20,
-                color: Colors.white.withOpacity(0.6),
-                child: Text(
-                  "Mecanica",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 18),
+                Container(
+                  padding: const EdgeInsets.only(
+                      top: 140, bottom: 0, left: 0, right: 0),
+                  child: Stack(children: [
+                    Container(
+                      width: 190,
+                      height: 20,
+                      color: Colors.white.withOpacity(0.6),
+                      child: Text(
+                        "Mecanica",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    )
+                  ]),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/Mecanica.jpg"),
+                        fit: BoxFit.cover),
+                  ),
                 ),
-              )
-            ]),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage("assets/Mecanica.jpg"), fit: BoxFit.cover),
-            ),
-          ),
-          Container(
-            padding:
-                const EdgeInsets.only(top: 140, bottom: 0, left: 0, right: 0),
-            child: Stack(children: [
-              Container(
-                width: 190,
-                height: 20,
-                color: Colors.white.withOpacity(0.6),
-                child: Text(
-                  "Electrica y Neumatica",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 18),
+                Container(
+                  padding: const EdgeInsets.only(
+                      top: 140, bottom: 0, left: 0, right: 0),
+                  child: Stack(children: [
+                    Container(
+                      width: 190,
+                      height: 20,
+                      color: Colors.white.withOpacity(0.6),
+                      child: Text(
+                        "Electrica y Neumatica",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    )
+                  ]),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/Electrica.jpg"),
+                        fit: BoxFit.cover),
+                  ),
                 ),
-              )
-            ]),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage("assets/Electrica.jpg"), fit: BoxFit.cover),
-            ),
-          ),
-          Container(
-            padding:
-                const EdgeInsets.only(top: 140, bottom: 0, left: 0, right: 0),
-            child: Stack(children: [
-              Container(
-                width: 190,
-                height: 20,
-                color: Colors.white.withOpacity(0.6),
-                child: Text(
-                  "Lego Mindstorms",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 18),
+                Container(
+                  padding: const EdgeInsets.only(
+                      top: 140, bottom: 0, left: 0, right: 0),
+                  child: Stack(children: [
+                    Container(
+                      width: 190,
+                      height: 20,
+                      color: Colors.white.withOpacity(0.6),
+                      child: Text(
+                        "Lego Mindstorms",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    )
+                  ]),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/Lego.jpeg"),
+                        fit: BoxFit.cover),
+                  ),
                 ),
-              )
-            ]),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage("assets/Lego.jpeg"), fit: BoxFit.cover),
-            ),
-          ),
-          Container(
-            padding:
-                const EdgeInsets.only(top: 140, bottom: 0, left: 0, right: 0),
-            child: Stack(children: [
-              Container(
-                width: 190,
-                height: 20,
-                color: Colors.white.withOpacity(0.6),
-                child: Text(
-                  "Viajes",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 18),
+                Container(
+                  padding: const EdgeInsets.only(
+                      top: 140, bottom: 0, left: 0, right: 0),
+                  child: Stack(children: [
+                    Container(
+                      width: 190,
+                      height: 20,
+                      color: Colors.white.withOpacity(0.6),
+                      child: Text(
+                        "Viajes",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    )
+                  ]),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/Otros.jpeg"),
+                        fit: BoxFit.cover),
+                  ),
                 ),
-              )
-            ]),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage("assets/Otros.jpeg"), fit: BoxFit.cover),
-            ),
-          ),
-        ],
-      )),
-    );
+              ],
+            )),
+          ],
+        ));
   }
 }

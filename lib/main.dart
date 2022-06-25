30eabd4f84f5f9820ec4d9c9dcaa2e427eb8b2b3
Jocelyn Velarde <a01285780@itesm.mx>
2022-06-24 19:47:18 -0500

    added gesture detector with ontap

--- lib/main.dart
+++ lib/main.dart
@@ -10,21 +10,31 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.pink,
-      ),
-      home: MyGridScreen(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+
+          primarySwatch: Colors.pink,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("VOLTEC App Inventario"),
+            actions: [
+              IconButton(
+                  icon: Icon(Icons.account_box_rounded), onPressed: () {}),
+            ],
+            backgroundColor: const Color.fromARGB(255, 12, 107, 185),
+          ),
+          body: MyGridScreen(),
+        ));
   }
 }

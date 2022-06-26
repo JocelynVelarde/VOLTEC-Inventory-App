3a972bb46716ff136808121838deb7c6f48fffde
Jocelyn Velarde <a01285780@itesm.mx>
2022-06-26 17:54:25 -0500

    simple drawer for homepage

--- lib/main.dart
+++ lib/main.dart
@@ -26,8 +26,49 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.pink,
         ),
         home: Scaffold(
+          drawer: Drawer(
+            child: Container(
+              color: Color.fromARGB(255, 109, 166, 213),
+              child: Column(
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    margin: const EdgeInsets.only(top: 50, bottom: 20),
+                    child: Image.asset("assets/Stitch.png"),
+                  ),
+                  const Text(
+                    "Jocelyn Velarde",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 30),
+                    padding: const EdgeInsets.all(20),
+                    width: double.infinity,
+                    color: Colors.white.withOpacity(0.6),
+                    child: const Text("Home"),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 10),
+                    padding: const EdgeInsets.all(20),
+                    width: double.infinity,
+                    color: Colors.white.withOpacity(0.6),
+                    child: const Text("Login"),
+                  ),
+                  Expanded(child: Container()),
+                  Container(
+                    margin: const EdgeInsets.only(top: 2),
+                    padding: const EdgeInsets.all(20),
+                    width: double.infinity,
+                    color: Colors.white.withOpacity(0.6),
+                    child: const Text("Logout"),
+                  ),
+                ],
+              ),
+            ),
+          ),
           appBar: AppBar(
-            title: Text("VOLTEC App Inventario"),
+            title: const Text("VOLTEC App Inventario"),
             actions: [
               IconButton(
                   icon: Icon(Icons.account_box_rounded),
@@ -37,7 +78,7 @@ class MyApp extends StatelessWidget {
             ],
             backgroundColor: const Color.fromARGB(255, 12, 107, 185),
           ),
-          body: MyGridScreen(),
+          body: const MyGridScreen(),
         ));
   }
 }

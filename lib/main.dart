dec9331c845e285d32ea8a88a62b341d06d921c4
Jocelyn Velarde <a01285780@itesm.mx>
2022-06-28 19:21:04 -0500

    3wqds

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app_la_buena/Appbars.dart';
 import 'package:app_la_buena/MyGridScreen.dart';
 import 'package:flutter/material.dart';
 
@@ -26,73 +27,7 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
         ),
         home: Scaffold(
-          drawer: Drawer(
-            child: ListView(
-              padding: EdgeInsets.zero,
-              children: [
-                UserAccountsDrawerHeader(
-                  accountName: Text(
-                    'Jocelyn Velarde',
-                    style:
-                        TextStyle(color: Colors.black, fontFamily: 'Raleway'),
-                  ),
-                  accountEmail: Text(
-                    'a01285780@tec.mx',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                  currentAccountPicture: CircleAvatar(
-                    child: ClipOval(
-                      child: Image.asset(
-                        'assets/Stitch.png',
-                        fit: BoxFit.cover,
-                        width: 90,
-                        height: 90,
-                      ),
-                    ),
-                  ),
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    image: DecorationImage(
-                        fit: BoxFit.fill,
-                        image: AssetImage('assets/Fondo.JPG')),
-                  ),
-                ),
-                ListTile(
-                  leading: Icon(Icons.home),
-                  title: Text('Home'),
-                  onTap: () => null,
-                ),
-                ListTile(
-                  leading: Icon(Icons.login),
-                  title: Text('Login'),
-                  onTap: () => null,
-                ),
-                ListTile(
-                  leading: Icon(Icons.share),
-                  title: Text('Screen'),
-                  onTap: () => null,
-                ),
-                Divider(),
-                ListTile(
-                  title: Text('Logout'),
-                  leading: Icon(Icons.logout),
-                  onTap: () => null,
-                ),
-              ],
-            ),
-          ),
-          appBar: AppBar(
-            title: const Text("VOLTEC App Inventario"),
-            actions: [
-              IconButton(
-                  icon: Icon(Icons.account_box_rounded),
-                  onPressed: () {
-                    print("icon");
-                  }),
-            ],
-            backgroundColor: const Color.fromARGB(255, 12, 107, 185),
-          ),
-          body: const MyGridScreen(),
+          body: Stack(children: [const MyGridScreen(), Appbars()]),
         ));
   }
 }

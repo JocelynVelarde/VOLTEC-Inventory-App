0999c1243769d8056902b83e5e1d9b7862ec4371
Jocelyn Velarde <a01285780@itesm.mx>
2022-06-29 00:06:13 -0500

    created data for the api

---
+++ lib/feedback_model.dart
@@ -0,0 +1,10 @@
+class FeedbackModel {
+  String foto;
+  String nombre;
+  String marca;
+  String cantidad;
+  String comprar;
+
+  FeedbackModel(
+      this.foto, this.nombre, this.marca, this.cantidad, this.comprar);
+}

207831ff45b26996b40c0fe3028a5e5256a95d3e
Jocelyn Velarde <a01285780@itesm.mx>
2022-07-12 20:11:19 -0500

    background image and correct display of the gridview with a button that is not currently working

--- lib/Herramientas.dart
+++ lib/Herramientas.dart
@@ -1,4 +1,5 @@
 import 'package:app_la_buena/form_controller.dart';
+import 'package:app_la_buena/run.dart';
 import 'package:flutter/material.dart';
 import 'package:app_la_buena/form.dart';
 
@@ -8,11 +9,12 @@ class FeedbackListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Feedback Responses',
+        debugShowCheckedModeBanner: false,
+        title: 'Inventario Herramientas',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: FeedbackListPage(title: "Responses"));
+        home: FeedbackListPage(title: "Inventario Herramientas"));
   }
 }
 
@@ -45,43 +47,85 @@ class _FeedbackListPageState extends State<FeedbackListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: GridView.builder(
-            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                maxCrossAxisExtent: 200, mainAxisSpacing: 10),
-            itemCount: feedbackItems.length,
-            itemBuilder: (context, index) {
-              return GridView.extent(
-                primary: false,
-                padding: const EdgeInsets.all(16),
-                crossAxisSpacing: 10,
-                maxCrossAxisExtent: 200.0,
-                children: <Widget>[
-                  Container(
-                    padding: const EdgeInsets.only(
-                        top: 140, bottom: 0, left: 0, right: 0),
-                    child: Stack(children: [
+        appBar: AppBar(
+          title: const Text("Inventario de Herramientas"),
+        ),
+        body: Stack(children: [
+          Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+              colorFilter: ColorFilter.mode(
+                  Colors.blueGrey.withOpacity(0.5), BlendMode.srcOver),
+              image: AssetImage(
+                "assets/Fondo.JPG",
+              ),
+              fit: BoxFit.cover,
+            )),
+            height: 700,
+            alignment: Alignment.bottomRight,
+            padding: const EdgeInsets.all(20),
+            child: CircleAvatar(
+              radius: 25,
+              backgroundColor: Colors.blue,
+              child: IconButton(
+                icon: const Icon(Icons.add),
+                color: Colors.white,
+                highlightColor: Colors.black,
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => run(),
+                      ));
+                },
+              ),
+            ),
+          ),
+          Center(
+            child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 250,
+                  mainAxisSpacing: 1 / 2,
+                  crossAxisSpacing: 1 / 2,
+                ),
+                itemCount: feedbackItems.length,
+                itemBuilder: (context, index) {
+                  return GridView.extent(
+                    primary: false,
+                    padding: const EdgeInsets.all(16),
+                    mainAxisSpacing: 1 / 2,
+                    crossAxisSpacing: 1 / 2,
+                    maxCrossAxisExtent: 250.0,
+                    children: <Widget>[
                       Container(
-                        width: 190,
-                        height: 20,
-                        color: Colors.white.withOpacity(0.6),
-                        child: Text(
-                          "${feedbackItems[index].name} (${feedbackItems[index].email})",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontSize: 18),
+                        padding: const EdgeInsets.only(
+                            top: 140, bottom: 0, left: 0, right: 0),
+                        child: Stack(children: [
+                          Container(
+                            width: 190,
+                            height: 20,
+                            color: Colors.white.withOpacity(0.6),
+                            child: Text(
+                              "${feedbackItems[index].name} (${feedbackItems[index].email})",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          )
+                        ]),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(14.0)),
+                          image: DecorationImage(
+                              image: AssetImage(
+                                "assets/Herramientas.jpeg",
+                              ),
+                              fit: BoxFit.cover),
                         ),
                       )
-                    ]),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(14.0)),
-                      image: DecorationImage(
-                          image: AssetImage(
-                            "assets/Herramientas.jpeg",
-                          ),
-                          fit: BoxFit.cover),
-                    ),
-                  )
-                ],
-              );
-            }));
+                    ],
+                  );
+                  /**/
+                }),
+          ),
+        ]));
   }
 }
